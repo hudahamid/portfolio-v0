25c3edfb819f21b9c939982a795855d97a04d63d
hudahamid <huda.00091581@gmail.com>
2023-04-08 12:50:59 +0300

    services

---
+++ src/component/Servicess/Services.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Services=() =>{
+  return (
+    <div  className="services">
+     <div className='awesome'>
+    <span> My Awesome </span>
+    <span>services</span>
+    <spane>
+    Lorem ispum is simpley dummy 
+    text of printing of printing Lorem
+    <br/>
+    ispum is simpley dummy text of printing
+    </spane>
+    <a>
+     <button>Download My Resume</button>   
+    </a>
+     </div>
+    </div>
+  )
+}
+
+export default Services
